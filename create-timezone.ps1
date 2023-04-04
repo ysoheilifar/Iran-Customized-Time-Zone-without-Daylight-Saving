d80dc71a68dc65107819ddf5388e2d87dbda700a
Yaser Soheilifar <83553075+ysoheilifar@users.noreply.github.com>
2023-04-04 22:24:24 +0330

    Add files via upload

---
+++ create-timezone.ps1
@@ -0,0 +1,28 @@
+$key_path = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Time Zones"
+$key_name = "IR Standard Time"
+$display_name = "(UTC+03:30) IR Standard Time"
+$dlt = "IR Daylight Time"
+$std = "IR Standard Time"
+$mui_display = "-"
+$mui_dlt = "-"
+$mui_std = "-"
+
+# Create the new time zone key and subkeys
+New-Item -Path "$key_path\$key_name" -Force | Out-Null
+
+# Set the display name for the new time zone
+Set-ItemProperty -Path "$key_path\$key_name" -Name "Display" -Value $display_name
+Set-ItemProperty -Path "$key_path\$key_name" -Name "MUI_Display" -Value $mui_display
+
+# Set the standard and daylight saving time values
+Set-ItemProperty -Path "$key_path\$key_name" -Name "Dlt" -Value $dlt
+Set-ItemProperty -Path "$key_path\$key_name" -Name "Std" -Value $std
+Set-ItemProperty -Path "$key_path\$key_name" -Name "MUI_Dlt" -Value $mui_dlt
+Set-ItemProperty -Path "$key_path\$key_name" -Name "MUI_Std" -Value $mui_std
+
+$tziHexValue = "2e-ff-ff-ff-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00"
+$byteArray = $tziHexValue -split "-" | ForEach-Object { [byte]([convert]::ToInt32($_, 16)) }
+New-ItemProperty -Path "$key_path\$key_name" -Name TZI -Value $byteArray -PropertyType Binary -Force | Out-Null
+
+# Set the time zone information using the tzutil command
+tzutil /s $key_name
